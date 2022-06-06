9ada88a51580b4d66517d3a36fc0352a97ee419a
Cugry <knownerx@gmail.com>
2022-06-06 17:17:48 +0900

    Change PLODUCT_MODEL and PROP_OVERRIDES

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -10,7 +10,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := SM6250
+PRODUCT_MODEL := Redmi Note 9S
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="miatoll_global-user 11 RKQ1.200826.002 V12.5.5.0.RJWMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/miatoll_global/miatoll:11/RKQ1.200826.002/V12.5.5.0.RJWMIXM:user/release-keys
